c8ac236d38eba349079e82456838c70caf37dc75
Pablo Rodrigues Bertoldo Melo <pablobertoldo.melo@gmail.com>
2023-10-02 12:15:45 -0300

    Prática 10, exercício 01

---
+++ 10 - POO/CadastroVeiculos/cadastroveiculos/src/cadastramento/Main.java
@@ -0,0 +1,12 @@
+package cadastramento;
+
+public class Main {
+
+	public static void main(String[] args) {
+       Cadastro cadastro = new Cadastro();
+       cadastro.cadastrar();
+       cadastro.imprimir();
+       cadastro.encerrar();
+	}
+
+}
